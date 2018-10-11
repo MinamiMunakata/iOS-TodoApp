19064398d9d8fe3c57f74e7ed2a597850d73d849
Minami Munakata <june28munazo@gmail.com>
2018-10-11 08:57:38 -0700

    initial commit

---
+++ ToDoItems/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoItems
+//
+//  Created by minami on 2018-10-11.
+//  Copyright © 2018 宗像三奈美. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
