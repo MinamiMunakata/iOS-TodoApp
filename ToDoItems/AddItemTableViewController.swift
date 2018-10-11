3acae6faa58ac75f285c03fb42e32b3e7391a055
Minami Munakata <june28munazo@gmail.com>
2018-10-11 11:25:46 -0700

    disable 'done' button unless the user input something

--- ToDoItems/AddItemTableViewController.swift
+++ ToDoItems/AddItemTableViewController.swift
@@ -12,16 +12,25 @@ class AddItemTableViewController: UITableViewController {
     @IBOutlet weak var doneBarButton: UIBarButtonItem!
     @IBOutlet weak var cancelBarButton: UIBarButtonItem!
     @IBAction func done(_ sender: UIBarButtonItem) {
+        
     }
     @IBAction func cancel(_ sender: UIBarButtonItem) {
+        navigationController?.popViewController(animated: true)
     }
     
     @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        textField.delegate = self
+        // you can do them below in storyboard
+//        textField.delegate = self
+//        doneBarButton.isEnabled = false
         navigationItem.largeTitleDisplayMode = .never
         
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        // right before a user select the row
+        return nil // nothing gonna be selected
     }
     
     override func viewWillAppear(_ animated: Bool) { // after viewDidLoad and right before display view
@@ -34,5 +43,17 @@ extension AddItemTableViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // Unwrapping Optionals
+        // 1. if-let
+        // 2. guard
+        guard let oldText = textField.text, let stringRange = Range(range, in: oldText) else { return false }
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        doneBarButton.isEnabled = newText.isEmpty ? false : true
+        
+        return true
+
     }
 }
