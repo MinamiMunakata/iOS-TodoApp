21c7175742d4b22c53c147c589ed00bfbaeeca16
Minami Munakata <june28munazo@gmail.com>
2018-10-11 09:28:07 -0700

    defined tableViewDataSource and set todoLabel and checkmark with tag

--- ToDoItems/TodosViewController.swift
+++ ToDoItems/TodosViewController.swift
@@ -12,7 +12,26 @@ class TodosViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    // tableViewDataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // num of rows
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // how each cell looks like
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItem", for: indexPath)
+        // checkmark, todoLabel
+        if let checkmark = cell.viewWithTag(11) as? UILabel {
+            checkmark.text = ""
+        }
+        
+        if let todoLabel = cell.viewWithTag(10) as? UILabel {
+            todoLabel.text = "Study iOS development"
+        }
+        return cell
     }
 
 
